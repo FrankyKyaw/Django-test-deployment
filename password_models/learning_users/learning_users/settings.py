05c4b77930b7768fcbc8ecb825595888077b808d
Franky <myothetkyaw@bennington.edu>
2022-11-12 21:19:35 -0500

    changes

--- password_models/learning_users/learning_users/settings.py
+++ password_models/learning_users/learning_users/settings.py
@@ -23,7 +23,7 @@ MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+SECRET_KEY = 'django-insecure-4$a5%il=pevhrto+wn!7_)1_h@yqqawr)7=&5dcd1e!*trr9*1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
